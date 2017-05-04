fb2864f066ca6c48c8c3254d86efd7eddd6258c1
Marwan Saleh <marwan@bsminsbroker.com>
2017-05-04 09:57:47 +0700

    Update dokumen upload

--- engine/application/config/production/config.php
+++ engine/application/config/production/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://www.indonesiasatu.co/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
