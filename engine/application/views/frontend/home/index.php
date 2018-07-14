9c82affb816aed3cd06ec5a8dc11f8404131c6c2
Marwan Saleh <marwan@bsminsbroker.com>
2018-07-14 19:26:25 +0700

    Edit bottom most advert

--- engine/application/views/frontend/home/index.php
+++ engine/application/views/frontend/home/index.php
@@ -252,47 +252,7 @@
         <?php endif; ?>
     </div>
     <?php endif; ?>
-    
-    <?php if (isset($category_hiburan)):?>
-    <div class="col-sm-6 article-box">
-        <div class="box-title">
-            <h2><?php echo $category_hiburan->name; ?></h2>
-            <div class="title-line"></div>
-        </div>
-        <?php if ($category_hiburan->articles): ?>
-        <div class="articles-slider">
-            <div class="flex-viewport">
-                <?php $i=1; foreach ($category_hiburan->articles as $article): ?>
-
-                <?php if ($i==1): ?>
-                <div class="main-article">
-                    <div class="title">
-                        <span><a href="<?php echo $article->url_short ? $article->url_short : site_url('detail/'.$article->url_title); ?>"><?php echo $article->title; ?></a></span>
-                    </div>
-                    <figure>
-                        <img class="medium" src="<?php echo get_image_thumb($article->image_url, IMAGE_THUMB_MEDIUM); ?>" alt="">
-                    </figure>
-                    <div class="main-text">
-                        <div class="inner">
-                            <span class="article-info"><?php echo date('d/m/Y',$article->date); ?></span>
-                            <p><?php echo $article->synopsis; ?> <a href="<?php echo site_url('detail/'.$article->url_title); ?>">Read more...</a></p>
-                        </div>
-                    </div>
-                </div>
-                <?php else: ?>
-                <article>
-                    <figure style="overflow:hidden;"><img class="img-responsive" src="<?php echo get_image_thumb($article->image_url, IMAGE_THUMB_SMALL); ?>" alt=""></figure>
-                    <div class="text">
-                        <h3><a href="<?php echo $article->url_short ? $article->url_short : site_url('detail/'.$article->url_title); ?>"><?php echo $article->title; ?></a></h3>
-                        <span class="info"><?php echo date('d/m/Y',$article->date); ?></span>
-                    </div>
-                </article>
-                <?php endif; ?>
-                <?php $i++; endforeach; ?>
-            </div>
-        </div>
-        <?php endif; ?>
-    </div>
-    <?php endif; ?>
 </div>
-
+<?php if ($adverts && isset($adverts[ADV_TYPE_HOME_BOTTOM])): ?>
+<?php $this->load->view('frontend/advert/home_bottom'); ?>
+<?php endif; 
