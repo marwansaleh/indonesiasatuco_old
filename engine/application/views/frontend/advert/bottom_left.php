db33b9d1d3c7ef39a25698749f7cc5dfea042903
Marwan Saleh <marwan@bsminsbroker.com>
2018-07-14 18:52:52 +0700

    Edit bottom left advert

--- engine/application/views/frontend/advert/bottom_left.php
+++ engine/application/views/frontend/advert/bottom_left.php
@@ -1,4 +1,6 @@
 <?php if ($adverts && isset($adverts[ADV_TYPE_BOTTOM_LEFT])): ?>
+
+<?php if (count($adverts[ADV_TYPE_BOTTOM_LEFT])>1): ?>
 <div class="row" style="margin-top:5px;">
     <div class="col-sm-12  <?php echo count($adverts[ADV_TYPE_BOTTOM_LEFT])>1?'flexslider':''; ?>" style="margin-bottom:5px;" id="advert-botom-left-desktop">
         <ul class="slides">
@@ -17,9 +19,6 @@
     </div>
     
 </div>
-<?php endif; ?>
-
-<?php if (count($adverts[ADV_TYPE_BOTTOM_LEFT])>1): ?>
 <script type="text/javascript">
     $(document).ready(function (){
         $('#advert-botom-left-desktop').flexslider({
@@ -32,4 +31,21 @@
         });
     });
 </script>
-<?php endif; 
+
+<?php else: ?>
+<div class="row" style="margin-top:5px;">
+    <?php foreach ($adverts[ADV_TYPE_BOTTOM_LEFT] as $adv): ?>
+    <div class="col-sm-12" style="margin-bottom:5px;">
+        <?php if ($adv->link_url && $adv->link_url != '#'): ?>
+        <a href="<?php echo site_url('click/run/'.$adv->id.'/'.  urlencode(base64_encode($adv->link_url))); ?>" <?php echo $adv->new_window==1?'target="blank"':''; ?>>
+            <img class="img-responsive" src="<?php echo $adv->file_name; ?>">
+        </a>
+        <?php else: ?>
+        <img class="img-responsive" src="<?php echo $adv->file_name; ?>">
+        <?php endif; ?>
+    </div>
+    <?php endforeach; ?>
+</div>
+<?php endif; ?>
+
+<?php endif;
